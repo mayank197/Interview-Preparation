a9dd9b90dfea849a70c35f715f6d38ee7d3ab05e
mayank197 <mayank.1996.2014@gmail.com>
2019-09-18 23:54:52 +0530

    DP Ques 1 - Added Approaches

--- Dynamic Programming/ Find all N-digit binary strings without consecutive 1's.cpp
+++ Dynamic Programming/ Find all N-digit binary strings without consecutive 1's.cpp
@@ -1,7 +1,8 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int countStrings(int n, int last_digit){
+// The Recursive Approach
+int countStrings_Recursive(int n, int last_digit){
 	
 	if(n==0) return 0;
 	
@@ -14,17 +15,42 @@ int countStrings(int n, int last_digit){
 	}
 	
 	if(last_digit==0)
-		return countStrings(n-1,0) + countStrings(n-1,1);
+		return countStrings_Recursive(n-1,0) + countStrings_Recursive(n-1,1);
 	
-	return countStrings(n-1,0);
+	return countStrings_Recursive(n-1,0);
+}
+
+// DP Approach
+int countStrings_DP(int n){
+	
+	// Here dp[i][j] will represent number of 'i' digit strings with 'j' as last digit 
+	
+	int dp[n+1][2];		
+	
+	dp[0][0] = 0;					
+	dp[0][1] = 0;					
+	
+	dp[1][0] = 2;		// There can be two 1-digit strings : "0" and "1"
+	dp[1][1] = 1;     // If 1 is the last digit, we can have only one string "1"
+	
+	for(int i=2;i<=n;i++){
+		
+		dp[i][0] = dp[i-1][0] + dp[i-1][1];
+		
+		dp[i][1] = dp[i-1][0];
+		
+	}
+	return dp[n][0];		
 }
 
 int main(){
 	
 	int n;
+	cout<<"\nEnter n : ";
 	cin>>n;
 	
-	cout<<"\nNumber of "<<n<<" digit binary strings without consecutive 1's are : ";
-	cout<<countStrings(n,0);		// Sending last digit as 0
+	cout<<"Number of "<<n<<" digit binary strings without consecutive 1's are : ";
+	// cout<<countStrings_Recursive(n,0);		// Recursive 
+	cout<<countStrings_DP(n);					// DP
 	
 }
